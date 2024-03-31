45ee0a7e9b10e7ce08af27ead9b0106310b0325e
admin <phentalex@gmail.com>
2024-03-31 19:27:27 +0300

    Добавьте файлы проекта.

---
+++ Car-Rental/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace Car_Rental
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        string CS;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString.ToString();
+        }
+
+        MySqlConnection con = new MySqlConnection();
+
+    }
+}
